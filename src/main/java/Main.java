d748347eefe5b3582ee996c1b09aaf484254c906
LuZhengjie <1368099869@qq.com>
2022-12-07 11:26:34 +0800

    fix test-commit filter bug

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,17 +1,30 @@
 import Helper.RepoHelper;
 import Utils.RepoUtils;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.AnnotationDeclaration;
+import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.comments.Comment;
+import com.github.javaparser.ast.visitor.VoidVisitor;
+import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.patch.HunkHeader;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 public class Main {
+
+    public static final String GIT_PATH = "." + File.separator + "univocity-parsers" + File.separator + ".git";
     public static void main(String[] args) throws IOException, GitAPIException {
-        try (Repository repository = RepoHelper.openJGitRepository("./univocity-parsers/.git")) {
+
+        try (Repository repository = RepoHelper.openJGitRepository(GIT_PATH)) {
             RepoUtils repoUtil = new RepoUtils(repository);
-            repoUtil.showOnlyTestChangedCommits();
-//            repoUtil.showAllCommits();
+            System.out.println(repoUtil.showOnlyTestChangedCommits().size());
             repoUtil.close();
         }
+
     }
 }
--- src/main/java/Utils/RepoUtils.java
+++ src/main/java/Utils/RepoUtils.java
@@ -2,47 +2,54 @@ package Utils;
 
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.api.ListBranchCommand;
-import org.eclipse.jgit.api.LogCommand;
 import org.eclipse.jgit.api.Status;
 import org.eclipse.jgit.api.errors.GitAPIException;
 import org.eclipse.jgit.diff.DiffEntry;
+import org.eclipse.jgit.diff.DiffFormatter;
+import org.eclipse.jgit.diff.Edit;
 import org.eclipse.jgit.lib.ObjectId;
 import org.eclipse.jgit.lib.ObjectReader;
 import org.eclipse.jgit.lib.Ref;
 import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.patch.FileHeader;
+import org.eclipse.jgit.patch.HunkHeader;
 import org.eclipse.jgit.revwalk.RevCommit;
-import org.eclipse.jgit.revwalk.RevWalk;
 import org.eclipse.jgit.treewalk.CanonicalTreeParser;
+import org.eclipse.jgit.util.io.DisabledOutputStream;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class RepoUtils {
     private final Repository repository;
     private final Git git;
+    private final CanonicalTreeParser parentTreeParser;
+    private final CanonicalTreeParser childTreeParser;
 
     public RepoUtils(Repository repository) throws IOException {
         this.repository = repository;
         git = new Git(repository);
-//        System.out.println("Having repo: " + repository.getDirectory());
-//        Ref head = repository.exactRef("refs/heads/master");
-//        System.out.println("Ref of refs/heads/master: " + head);
+        parentTreeParser = new CanonicalTreeParser();
+        childTreeParser = new CanonicalTreeParser();
     }
 
     public void close() {
         git.close();
     }
 
-    public void listTags() throws GitAPIException, IOException {
+    @SuppressWarnings("unused")
+    public void listTags() throws GitAPIException {
         List<Ref> tagRefList = git.tagList().call();
         for (var ref : tagRefList) {
             System.out.println("Tag: " + ref + " " + ref.getName() + " " + ref.getObjectId().getName() +
                     (ref.getPeeledObjectId() == null ? "" : ", peeled: " + ref.getPeeledObjectId()));
         }
-
     }
 
     //list all branches of this repository
+    @SuppressWarnings("unused")
     public void listBranches() throws GitAPIException {
         List<Ref> brhRefList = git.branchList().call();
         for (var ref : brhRefList) {
@@ -55,6 +62,7 @@ public class RepoUtils {
         }
     }
 
+    @SuppressWarnings("unused")
     public void showAllCommits() throws GitAPIException, IOException {
         Iterable<RevCommit> commits = git.log().all().call();
         int cnt = 0;
@@ -65,45 +73,57 @@ public class RepoUtils {
         System.out.println(cnt + " commits in total");
     }
 
-    public void showOnlyTestChangedCommits() throws IOException, GitAPIException {
+    //todo give DiffEntry out
+    public List<RevCommit> showOnlyTestChangedCommits() throws IOException, GitAPIException {
+        //get all commits
         Iterable<RevCommit> commits = git.log().all().call();
         int cnt = 0;
-        RevCommit lastCommit = null;
-        CanonicalTreeParser lastParser = new CanonicalTreeParser();
-        CanonicalTreeParser newParser = new CanonicalTreeParser();
-        int count = 0;
-        boolean onlyTestChanged = true;
-        try (ObjectReader reader = repository.newObjectReader()) {
-            for (var commit : commits) {
-                List<DiffEntry> diffs;
-                newParser.reset(reader, commit.getTree());
-                if (lastCommit == null) {
-                    diffs = git.diff().setNewTree(newParser).setOldTree(null).call();
-                } else {
-                    lastParser.reset(reader, lastCommit.getTree());
-                    diffs = git.diff().setNewTree(newParser).setOldTree(lastParser).call();
-                }
-
-                for (var entry : diffs) {
-//                    System.out.println("entry: " + entry);
-                    if (!entry.toString().contains("src/test/")) {
-                        onlyTestChanged = false;
-                    }
-                }
-                System.out.println("--------------------");
-                if (onlyTestChanged && diffs.size() > 0) {
-                    for (var entry : diffs) {
-                        System.out.println(/*"old: " + entry.getOldPath() + ", new: " + entry.getNewPath() + */"Entry: " + entry);
-                    }
-                    count++;
-                }
-
-                cnt++;
-                lastCommit = commit;
+        int onlyTestChangedCnt = 0;
+        List<RevCommit> commitList = new ArrayList<>();
+        for (var commit : commits) {//reverse order
+            if (commit.getParentCount() <= 0) {
+                continue;
             }
-            System.out.println(cnt + " commits in total");
-            System.out.println(count + " test commits");
+            //todo may deal with merge commit
+            RevCommit parentCommit = commit.getParent(0);
+            List<DiffEntry> diffs = getDiffBetweenCommits(parentCommit, commit);
+
+            System.out.println("--------------------");
+            if (onlyTestChanged(diffs)) {
+                commitList.add(commit);
+                for (var entry : diffs) {
+                    System.out.println(/*"old: " + entry.getOldPath() + ", new: " + entry.getNewPath() + */"Entry: " + entry);
+                }
+                onlyTestChangedCnt++;
+            }
+            cnt++;
         }
+        System.out.println(cnt + " commits in total");
+        System.out.println(onlyTestChangedCnt + " test commits");
+        return commitList;
+    }
+
+    public List<DiffEntry> getDiffBetweenCommits(RevCommit parentCommit, RevCommit childCommit) throws IOException, GitAPIException {
+        ObjectId parentId = parentCommit.getTree().getId();
+        ObjectId childId = childCommit.getTree().getId();
+        List<DiffEntry> diffs;
+        try (ObjectReader reader = repository.newObjectReader()) {
+            parentTreeParser.reset(reader, parentId);
+            childTreeParser.reset(reader, childId);
+            diffs = git.diff().setNewTree(childTreeParser).setOldTree(parentTreeParser).call();
+        }
+        return diffs;
+    }
+
+    //todo modify
+    private static boolean onlyTestChanged(List<DiffEntry> diffs) {
+        boolean onlyTestChanged = true;
+        for (var entry : diffs) {
+            if (!entry.toString().contains("src/test")) {
+                onlyTestChanged = false;
+            }
+        }
+        return onlyTestChanged && diffs.size() > 0;
     }
 
     @SuppressWarnings("unused")
@@ -124,6 +144,7 @@ public class RepoUtils {
         }
     }
 
+    @SuppressWarnings("unused")
     public void showStatus() throws GitAPIException {
         Status status = git.status().call();
         System.out.println("Added: " + status.getAdded());
@@ -136,5 +157,16 @@ public class RepoUtils {
         System.out.println("Removed: " + status.getRemoved());
         System.out.println("Untracked: " + status.getUntracked());
         System.out.println("UntrackedFolders: " + status.getUntrackedFolders());
+    }
+
+    private List<Edit> getEdit(DiffEntry entry) throws IOException {
+        List<Edit> editList = new LinkedList<>();
+        try (DiffFormatter diffFormatter = new DiffFormatter(DisabledOutputStream.INSTANCE)) {
+            diffFormatter.setRepository(repository);
+            FileHeader fileHeader = diffFormatter.toFileHeader(entry);
+            List <? extends HunkHeader> hunkHeaders = fileHeader.getHunks();
+            hunkHeaders.forEach(hunkHeader -> editList.addAll(hunkHeader.toEditList()));
+        }
+        return editList;
     }
 }
